a4170985ba9ee522457a8be65bdb320fd6afda0e
Austin Gill <notgnoshi@gmail.com>
2019-03-10 17:39:17 -0600

    Initial stab in the dark at an EA.

--- evolve/ea.py
+++ evolve/ea.py
@@ -1,30 +1,34 @@
 import numpy as np
 
+from evolve import fitness
 from evolve.shapes import NumpyCircleArray
 
 
 class EvolutionaryAlgorithm:
     """Implements an EA to approximate a given image."""
 
-    def __init__(self, shape, pop_size, ind_size):
+    def __init__(self, image, pop_size, ind_size):
         """Initialize the EA.
 
-        :param number: The number of circles to use
-        :param shape: The shape of the approximated image
-        :type shape: a (height, width) tuple of ints
+        :param image: The image to approximate
+        :type image: A 2d array of uint8's
         :param pop_size: The number of individuals in the population.
         :param ind_size: The number of circles composing an individual.
         """
-        self.height, self.width = shape
+        self.target = image
+        self.height, self.width = image.shape
         self.pop_size = pop_size
         self.ind_size = ind_size
 
         self.population = np.zeros((pop_size, ind_size), dtype=NumpyCircleArray.CircleDtype)
+        self.fitnesses = np.zeros(pop_size)
+
+        self.mutations = np.zeros_like(self.population)
+        self.children = None
+
         # TODO: If we ever attempt to perform parallel computation, each process will need its own
         # image represented by a single individual.
-        self.image = np.zeros(shape, dtype="uint8")
-
-        self.init_pop()
+        self.approx = np.zeros(image.shape, dtype="uint8")
 
     def init_pop(self):
         """Randomly initialize the population."""
@@ -71,5 +75,82 @@ class EvolutionaryAlgorithm:
             cx, cy, r = int(circle["center"]["x"]), int(circle["center"]["y"]), circle["radius"]
 
             x, y = np.ogrid[-cy : height - cy, -cx : width - cx]
-            mask = x**2 + y**2 <= r**2
+            mask = x ** 2 + y ** 2 <= r ** 2
             image[mask] = (image[mask] + circle["color"]) / 2
+
+    def evaluate(self):
+        """Update the population fitnesses."""
+        # TODO: This is a good candidate for parallelism.
+        for i in range(self.pop_size):
+            individual = self.population[i]
+            # TODO: Fill with the mean color of the target image?
+            self.compute_image(self.approx, individual, fill_color=self.target.mean())
+            self.fitnesses[i] = fitness(self.approx, self.target)
+
+    def perturb_radius(self, circle, scale=5):
+        """Perturb the radius of the given circle."""
+        dr = np.random.normal(scale=scale)
+        # Avoid overflows.
+        if dr < 0 and abs(dr) > circle["radius"]:
+            dr = -dr
+        circle["radius"] = circle["radius"] + dr
+
+    def perturb_color(self, circle, scale=5):
+        """Perturb the color of the given circle."""
+        dc = np.random.normal(scale=scale)
+        # Avoid overflows.
+        if dc < 0 and -dc > circle["color"]:
+            dc = -dc
+        circle["color"] += dc
+
+    def perturb_center(self, circle, scale=5):
+        """Perturb the center of the given circle."""
+        dx, dy = np.random.normal(scale=scale, size=2)
+        circle["center"]["x"] = (circle["center"]["x"] + dx) % self.width
+        circle["center"]["y"] = (circle["center"]["y"] + dy) % self.height
+
+    def mutate_individual(self, individual):
+        """Mutate the given individual in place."""
+        for circle in individual:
+            self.perturb_radius(circle)
+            self.perturb_color(circle)
+            self.perturb_center(circle)
+
+    def mutate(self):
+        """Mutate each individual in the population."""
+        np.copyto(self.mutations, self.population)
+        for mutant in self.mutations:
+            self.mutate_individual(mutant)
+
+    def reproduce(self):
+        """Reproduce the individuals in the population."""
+        # TODO: Should the population be sorted by fitness?
+        raise NotImplementedError
+
+    def select(self):
+        """Select the individuals who survive."""
+        raise NotImplementedError
+
+    def run(self, generations=100):
+        """Run the EA.
+
+        :param generations: The number of generations to run the EA for
+        :returns: The best individuals over the total runtime of the algorithm.
+        :rtype: An array of EvolutionaryAlgorithm.HistoryDtype objects.
+        """
+        self.init_pop()
+        self.evaluate()
+
+        fitnesses = np.zeros(generations)
+        individuals = np.zeros((generations, self.ind_size), dtype=NumpyCircleArray.CircleDtype)
+        for gen in range(generations):
+            # self.reproduce()
+            self.mutate()
+            # self.select()
+
+            self.evaluate()
+            best = np.argmax(self.fitnesses)
+            fitnesses[gen] = self.fitnesses[best]
+            individuals[gen] = self.population[best]
+
+        return fitnesses, individuals
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import imageio
 import matplotlib.pyplot as plt
+import numpy as np
 
 from evolve import EvolutionaryAlgorithm
 
@@ -8,13 +9,18 @@ from evolve import EvolutionaryAlgorithm
 def main():
     image = imageio.imread("images/test.png")
 
-    ea = EvolutionaryAlgorithm(image.shape, 5, 20)
+    ea = EvolutionaryAlgorithm(image, 5, 20)
 
-    EvolutionaryAlgorithm.compute_image(ea.image, ea.population[0])
+    fitnesses, individuals = ea.run(generations=100)
+
+    solution = individuals[np.argmax(fitnesses)]
+
+    approximation = np.zeros(image.shape, dtype="uint8")
+    ea.compute_image(approximation, solution, fill_color=image.mean())
 
     plt.title("Final Approximated Image")
     plt.axis("off")
-    plt.imshow(ea.image, cmap="gray", vmin=0, vmax=255)
+    plt.imshow(approximation, cmap="gray", vmin=0, vmax=255)
     plt.show()
 
 
