Fixed overflow issues with the image comparison

evolve/ea.py
@@ -31,7 +31,7 @@ class EvolutionaryAlgorithm:
 
         # TODO: If we ever attempt to perform parallel computation, each process will need its own
         # image represented by a single individual.
-        self.approx = np.zeros(image.shape, dtype="uint8")
+        self.approx = np.zeros(image.shape, dtype="int16")
 
     def init_pop(self):
         """Randomly initialize the population."""
@@ -129,12 +129,16 @@ class EvolutionaryAlgorithm:
         circle["center"]["x"] = (circle["center"]["x"] + dx) % self.width
         circle["center"]["y"] = (circle["center"]["y"] + dy) % self.height
 
-    def mutate_individual(self, individual, scale = 2):
+    def mutate_individual(self, individual, scale = 3):
         """Mutate the given individual in place."""
         for circle in individual:
-            self.perturb_radius(circle, scale)
-            self.perturb_color(circle, scale)
-            self.perturb_center(circle, scale)
+            choice = np.random.randint(0,3)
+            if choice == 0:
+                self.perturb_radius(circle, scale)
+            elif choice == 1:
+                self.perturb_color(circle, scale)
+            elif choice == 2:
+                self.perturb_center(circle, scale)
 
     def mutate(self):
         """Mutate each individual in the population."""
@@ -170,8 +174,8 @@ class EvolutionaryAlgorithm:
         joint = joint[indices]
         fit = fit[indices]
 
-        self.population = joint[self.pop_size:]
-        self.fitnesses = fit[self.pop_size:]
+        self.population = joint[:self.pop_size]
+        self.fitnesses = fit[:self.pop_size]
 
 
     # TODO: Display the current best individual as the EA progresses.
@@ -200,7 +204,7 @@ class EvolutionaryAlgorithm:
             self.select()
 
 
-            best = np.argmax(self.fitnesses)
+            best = np.argmin(self.fitnesses)
             if verbose:
                 print("best fitness:", self.fitnesses[best])
             fitnesses[gen] = self.fitnesses[best]

evolve/image.py
@@ -5,4 +5,5 @@ def fitness(image1, image2):
     """Determine how close two images are."""
     assert image1.shape == image2.shape
     height, width = image1.shape
-    return 1 / (np.sum((image1 - image2) ** 2) / (height * width))
+
+    return np.sum(np.abs(image1 - image2)) / (height * width)

test.py
@@ -31,18 +31,21 @@ def main(args):
     fitnesses, individuals = ea.run(generations=args.generations, verbose=not args.quiet)
 
     if not args.quiet:
-        solution = individuals[np.argmax(fitnesses)]
+        solution = individuals[np.argmin(fitnesses)]
 
         approximation = np.zeros(image.shape, dtype="uint8")
-        ea.compute_image(approximation, solution, fill_color=image.mean())
+        ea.compute_image(approximation, solution, 255)
 
-        _, axes = plt.subplots(1, 2)
+        _, axes = plt.subplots(1, 3)
         axes[0].set_title("Best Approximation")
         axes[0].imshow(approximation, cmap="gray", vmin=0, vmax=255)
         axes[0].axis("off")
         axes[1].set_title("Target Image")
         axes[1].imshow(image, cmap="gray", vmin=0, vmax=255)
         axes[1].axis("off")
+        axes[2].set_title("Diff")
+        axes[2].imshow(image - approximation, cmap="gray", vmin=0, vmax=255)
+        axes[2].axis("off")
         plt.show()
 
 