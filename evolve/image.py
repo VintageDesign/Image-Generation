4bb3561d3e1b0ca6e38a689182287a11e21e02b7
Austin Gill <notgnoshi@gmail.com>
2019-03-10 18:32:42 -0600

    Invert fitness and normalize by the area
    Invert the fitness so that larger values indicate images closer
    together. Normalize by the image area so that the inversion doesn't
    produce such small numbers.

--- evolve/image.py
+++ evolve/image.py
@@ -4,5 +4,5 @@ import numpy as np
 def fitness(image1, image2):
     """Determine how close two images are."""
     assert image1.shape == image2.shape
-    # TODO: Is normalization by the area necessary?
-    return np.sum((image1 - image2) ** 2)
+    height, width = image1.shape
+    return 1 / (np.sum((image1 - image2) ** 2) / (height * width))
