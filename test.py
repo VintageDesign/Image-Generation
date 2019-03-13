06a249e508aa275081f4f38f53df1b3fd8ca399a
Austin Gill <notgnoshi@gmail.com>
2019-03-12 20:03:14 -0600

    Revamp test script to handle multiple approaches.

--- test.py
+++ test.py
@@ -12,41 +12,61 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Approximate the given image using an EA.")
 
     parser.add_argument("--quiet", action="store_true", default=False, help="Quiet all output.")
-    parser.add_argument("image", help="The image to approximate.")
     parser.add_argument("--population", "-p", type=int, default=10, help="The population size.")
     parser.add_argument("--individual", "-i", type=int, default=20, help="The individual size.")
     parser.add_argument(
         "--generations", "-g", type=int, default=100, help="The number of generations."
     )
-    # TODO: Save the result to an output file.
+    parser.add_argument("image", help="The image to approximate.")
+    parser.add_argument("output", nargs="?", default="", help="The filename to save the output to.")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "--bootstrap",
+        action="store_true",
+        default=False,
+        help="Use the bootstrap method to build a quick approximation.",
+    )
+    group.add_argument(
+        "--ea", action="store_true", default=True, help="The default traditional, slow EA approach."
+    )
+    group.add_argument(
+        "--combined", action="store_true", default=False, help="A combined approach."
+    )
 
     return parser.parse_args()
 
 
 def main(args):
-    image = imageio.imread(args.image)
+    target = imageio.imread(args.image)
+    approximation = np.zeros_like(target, dtype="float32")
 
-    ea = EvolutionaryAlgorithm(image, pop_size=args.population, ind_size=args.individual)
+    if args.ea:
+        ea = EvolutionaryAlgorithm(target, pop_size=args.population, ind_size=args.individual)
+        fitnesses, individuals = ea.run(generations=args.generations, verbose=not args.quiet)
+        solution = individuals[np.argmin(fitnesses)]
+        print("best solution:", solution)
+        ea.compute_image(approximation, solution, 255)
+    elif args.bootstrap:
+        raise NotImplementedError
+    elif args.combined:
+        raise NotImplementedError
 
-    fitnesses, individuals = ea.run(generations=args.generations, verbose=not args.quiet)
+    _, axes = plt.subplots(1, 3)
+    axes[0].set_title("Best Approximation")
+    axes[0].imshow(approximation, cmap="gray", vmin=-255, vmax=255)
+    axes[0].axis("off")
+    axes[1].set_title("Target Image")
+    axes[1].imshow(target, cmap="gray", vmin=0, vmax=255)
+    axes[1].axis("off")
+    axes[2].set_title("Diff")
+    axes[2].imshow(np.abs(target - approximation), cmap="gray", vmin=0, vmax=255)
+    axes[2].axis("off")
+
+    if args.output:
+        plt.savefig(args.output)
 
     if not args.quiet:
-        solution = individuals[np.argmin(fitnesses)]
-        print(solution)
-
-        approximation = np.zeros(image.shape, dtype="float32")
-        ea.compute_image(approximation, solution, 255)
-
-        _, axes = plt.subplots(1, 3)
-        axes[0].set_title("Best Approximation")
-        axes[0].imshow(approximation, cmap="gray", vmin=-255, vmax=255)
-        axes[0].axis("off")
-        axes[1].set_title("Target Image")
-        axes[1].imshow(image, cmap="gray", vmin=0, vmax=255)
-        axes[1].axis("off")
-        axes[2].set_title("Diff")
-        axes[2].imshow(np.abs(image - approximation), cmap="gray", vmin=0, vmax=255)
-        axes[2].axis("off")
         plt.show()
 
 
